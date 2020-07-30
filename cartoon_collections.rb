516b43307eac992636e757a3318d92e7fafd1a4d
Luke Atencio <atencioluke@gmail.com>
2020-07-30 05:37:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.count { |word| word.length > 4 }
+  return true if words.count { |word| word.length > 4 } > 1
 end
 
 def find_the_cheese# code an argument here
