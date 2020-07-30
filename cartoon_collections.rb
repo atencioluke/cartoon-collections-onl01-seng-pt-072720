e1b808cd362801e759154378f35a0157c9692173
Luke Atencio <atencioluke@gmail.com>
2020-07-30 05:04:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarf_list = []
+  dwarves.map { |dwarf| dwarf }
+  return dwarves
 end
 
 def summon_captain_planet# code an argument here
