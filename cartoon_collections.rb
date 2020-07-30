ceb255e07ea26b239d1afd0aaba2d5a3fb1cc81b
Luke Atencio <atencioluke@gmail.com>
2020-07-30 05:46:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,5 +12,5 @@ end
 
 def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  foods.find { |food| food.include?(food)}
+  ingredients.find { |ingredient| cheese_types.include?(ingredient)}
 end
